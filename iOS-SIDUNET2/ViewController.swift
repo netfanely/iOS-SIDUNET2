6a69f4bbdf9844fcd1ec215bdaa23a187d56f5ee
Fanny Pastor Humpiri <fanely@netfanely.com>
2019-04-12 00:28:51 -0500

    Initial Commit

---
+++ iOS-SIDUNET2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-SIDUNET2
+//
+//  Created by Fanely on 4/12/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
